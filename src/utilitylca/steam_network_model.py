08c0ead25b6046ae68a5ffa5ef93977d8a92f8e9
Hannes Schneider <hannes.schneider@tu-braunschweig.de>
2025-07-02 10:45:59 +0200

    switch busses to powerconnections

--- src/utilitylca/steam_network_model.py
+++ src/utilitylca/steam_network_model.py
@@ -2,11 +2,13 @@ from tespy.components import (
     Turbine, Source, Sink, Pump, 
     Pipe, CycleCloser, SimpleHeatExchanger, Valve, Merge, 
     Splitter, Drum,
-    DropletSeparator
+    DropletSeparator,
+    PowerSink, PowerSource, Generator, Motor, PowerBus
 )
 from tespy.networks import Network
 from tespy.tools import ExergyAnalysis
-from tespy.connections import Connection, Ref, Bus
+from tespy.connections import Connection, Ref, Bus, PowerConnection
+
 import copy
 
 def create_steam_net(steam_lca):
@@ -17,17 +19,18 @@ def create_steam_net(steam_lca):
     steam_lca.nw = Network()
     steam_lca.nw.set_attr(iterinfo=False)
     steam_lca.nw.set_attr(T_unit='C', p_unit='bar', h_unit='kJ / kg')
+    # create components
     boiler = SimpleHeatExchanger('steam boiler' , dissipative=False)
     bpt = Turbine('back pressure turbine')
     pipe_warm =  Pipe('steam pipe', dissipative=True)
     cond_trap= DropletSeparator('condensate trap')
     valve = Valve('controlvalve')
-    heat_sink = SimpleHeatExchanger('heat sink', dissipative=False)
+    hex_heat_sink = SimpleHeatExchanger('heat sink', dissipative=False)
     pipe_cold= Pipe('condensate pipe',dissipative=True)
     feed_pump= Pump('feedpump')
     cycl=CycleCloser('CycleCloser')
     
-    steam_sink = Sink('steam losses')
+    steam_losses = Sink('steam losses')
     steam_leak= Splitter("steam leak")
     makeup_leak =Source('leak makeup')
     makeup_trap =Source('trap makeup')
@@ -39,23 +42,24 @@ def create_steam_net(steam_lca):
     split= Splitter("remove wastewater")
 
     condensate_split= Splitter("split condensate")
-    condensate_drum= Drum('condensate injection drum')
-    condensate_sink = Sink('sink 1')
+    #condensate_drum= Drum('condensate injection drum')
+    #condensate_sink = Sink('sink 1')
     
     dummy_sink2= Sink('dummy sink2')
     injection_source =Source('injection_source')
 
+    #create connections:
     c05 = Connection(cycl, 'out1', boiler, 'in1')
     c04 = Connection(boiler, 'out1', bpt, 'in1')
     c03 = Connection(bpt, 'out1', pipe_warm, 'in1')
     c022= Connection(pipe_warm, 'out1', steam_leak, 'in1')
     c02 = Connection(steam_leak, 'out1', valve, 'in1')
 
-    c_leak = Connection(steam_leak, 'out2', steam_sink, 'in1')
+    c_leak = Connection(steam_leak, 'out2', steam_losses, 'in1')
     muw2 = Connection(makeup_leak, 'out1', merge, 'in3')
 
-    c01= Connection(valve, 'out1', heat_sink, 'in1')
-    c1 = Connection(heat_sink, 'out1', pipe_cold, 'in1')
+    c01= Connection(valve, 'out1', hex_heat_sink, 'in1')
+    c1 = Connection(hex_heat_sink, 'out1', pipe_cold, 'in1')
     c2 = Connection(pipe_cold, 'out1', split, 'in1')
     c3 = Connection(split, 'out1', merge, 'in1')
     c4 = Connection(merge, 'out1', feed_pump, 'in1')
@@ -68,13 +72,15 @@ def create_steam_net(steam_lca):
                 c_leak, 
                 c1, c2, c3, c4, c5, 
                 muw, wawa, muw2)
-    
+    #set attributes:
     boiler.set_attr(pr = 1)
     c04.set_attr(fluid={"H2O": 1}, #fluid_engines={"H2O": IAPWSWrapper}, 
                     h= steam_lca.h_superheating_max_pressure)#Td_bp=100)
-    c05.set_attr(p0=steam_lca.main_pressure,m0=steam_lca.heat/2700E3 )
+    #c05.set_attr(p0=steam_lca.main_pressure,m0=steam_lca.heat/2700E3 )
     bpt.set_attr(eta_s = 0.85, )
-    c03.set_attr(p=steam_lca.main_pressure, h0=steam_lca.h_superheating_max_pressure, m0=steam_lca.heat/2700E3)
+    c03.set_attr(p=steam_lca.main_pressure, 
+                 #h0=steam_lca.h_superheating_max_pressure, m0=steam_lca.heat/2700E3
+                 )
     pipe_warm.set_attr(pr=0.95, 
         Tamb = steam_lca.Tamb, 
         kA= 100,
@@ -85,10 +91,14 @@ def create_steam_net(steam_lca):
         #wind_velocity=steam_lca.wind_velocity, environment_media = steam_lca.environment_media
             ) 
     c_leak.set_attr(m=Ref(c022, steam_lca.leakage_factor, 0))
-    c01.set_attr(p = steam_lca.needed_pressure,h0=steam_lca.h_superheating_max_pressure, m0=steam_lca.heat/2700E3)#T=needed_temperature +5,
+    c01.set_attr(p = steam_lca.needed_pressure,
+                 #h0=steam_lca.h_superheating_max_pressure, m0=steam_lca.heat/2700E3
+                 )#T=needed_temperature +5,
 
-    heat_sink.set_attr(pr=1,Q=-steam_lca.heat)
-    c1.set_attr(x=0,p0=steam_lca.needed_pressure,m0=steam_lca.heat/2700E3 )
+    hex_heat_sink.set_attr(pr=1,Q=-steam_lca.heat)
+    c1.set_attr(x=0,
+                #p0=steam_lca.needed_pressure,m0=steam_lca.heat/2700E3 
+                )
     pipe_cold.set_attr(pr=0.95, 
         Tamb = steam_lca.Tamb, kA= 300,
         L=steam_lca.pipe_length, D='var',  ks=4.57e-5,
@@ -98,9 +108,11 @@ def create_steam_net(steam_lca):
             )
     feed_pump.set_attr(eta_s =0.9)
 
-    c4.set_attr(p0=steam_lca.needed_pressure,m0=steam_lca.heat/2700E3 )
+    #c4.set_attr(p0=steam_lca.needed_pressure,m0=steam_lca.heat/2700E3 )
     
-    c5.set_attr(p=steam_lca.max_pressure, h0=steam_lca.h_superheating_max_pressure,m0=steam_lca.heat/2700E3 )
+    c5.set_attr(p=steam_lca.max_pressure, 
+                #h0=steam_lca.h_superheating_max_pressure,m0=steam_lca.heat/2700E3 
+                )
 
     muw.set_attr(m=Ref(c04, steam_lca.makeup_factor, 0), 
                     T=steam_lca.Tamb,
@@ -115,64 +127,66 @@ def create_steam_net(steam_lca):
     
     wawa.set_attr(m=Ref(c04, steam_lca.makeup_factor, 0))
     
-    # bus definition:
+    # create power connections:
     
-    fuel_bus = Bus('boiler bus')
-    fuel_bus.add_comps({'comp':boiler, 'base':'bus'})
+    #fuel_bus = PowerSource('boiler powersource')
+    #e_boil = PowerConnection(fuel_bus, 'power', boiler, 'heat', label= 'e_boil')
+    #fuel_bus.add_comps({'comp':boiler, 'base':'bus'})
     
-    turbine_bus = Bus('turbines')
-    turbine_bus.add_comps({'comp': bpt, 'base':'component','char': 0.97})
+    turbine_gen = Generator('turbines')
+    turbine_gen.set_attr(eta= 0.9)
+    turbine_grid = PowerSink('grid')
+    e_turb = PowerConnection(bpt, 'power', turbine_gen, 'power_in', label='e_turb')
+    e_turb_grid =PowerConnection(turbine_gen, 'power_out', turbine_grid, 'power', label='e_turb_grid')
 
-    pipe_emission =Bus('pipe losses')
-    
-    pipe_emission.add_comps({'comp':pipe_cold,'char': 1},
-                        {'comp':pipe_warm,'char': 1})
-    
-    pipe_fugitive_emission = Bus('fugitive emission')
-    pipe_fugitive_emission.add_comps({'comp': steam_sink,'base':'component'})
-    
-    product_bus =Bus('heat bus')
-    product_bus.add_comps({'comp':heat_sink, 'char': 1},
-                        )
+    #pipe_diss_sink =PowerSink('pipe dissipative losses sink')
+    #pipe_diss_bus = PowerBus('pipe dissipative losses bus', num_in =2, num_out=1)
 
-    pump_bus = Bus('feedpump bus')
-    pump_bus.add_comps({'comp':feed_pump, 'base':'bus'})
+    #e_pi_h = PowerConnection(pipe_warm, 'power', pipe_diss_bus, 'power_in1')
+    #e_pi_c = PowerConnection(pipe_cold, 'power', pipe_diss_bus, 'power_in2')
+    #e_pi_sink = PowerConnection(pipe_diss_bus, 'power', pipe_diss_sink, 'power', label='e_pi_sink')
+    
+    #heat_sink =PowerSink('heat sink')
+    #e_heat_sink =PowerConnection( hex_heat_sink,'heat',heat_sink, 'power', label='e_heat_sink')
 
-    makeup_bus= Bus('makeup water')
-    makeup_bus.add_comps({'comp':makeup, 'base':'bus'},
-                        {'comp':makeup_leak, 'base':'bus'},
-                        )
-    blowdown_bus=Bus('blowdown bus')
-    blowdown_bus.add_comps({'comp':blowdown, 'base':'component'})
+    pump_psource = PowerSource('feedpump powersource')
+    e_pump = PowerConnection(pump_psource, 'power', feed_pump, 'power', label='e_pump')
+
+
+    #makeup_bus= Bus('makeup water')
+    #makeup_bus.add_comps({'comp':makeup, 'base':'bus'},
+    #                    {'comp':makeup_leak, 'base':'bus'},
+    #                    )
+    #blowdown_bus=Bus('blowdown bus')
+    #blowdown_bus.add_comps({'comp':blowdown, 'base':'component'})
     #overall distribution losses of 20%: https://www.energy.gov/eere/iedo/manufacturing-energy-and-carbon-footprints-2018-mecs
     
-    steam_lca.nw.add_busses(turbine_bus, 
-                fuel_bus, 
-                product_bus, 
-                pipe_emission, 
-                #pipe_fugitive_emission,
-                makeup_bus,
-                blowdown_bus,
-                pump_bus)
+    steam_lca.nw.add_conns(#e_boil,
+        e_turb,e_turb_grid,
+                           #e_pi_c, e_pi_h, e_pi_sink,
+                           #e_heat_sink, 
+                           e_pump
+    )
+    
     steam_lca.nw.solve('design')
 
     #2. Run: 
     muw.set_attr(T=None)
     muw.set_attr(T=Ref(c2, 1, -20))
     
-    #steam_lca.nw.solve('design')
+    steam_lca.nw.solve('design')
 
     #3. Run: implement condensate injection:
     print(c022.x.val)
-    if c022.x.val ==-1:
+    if c022.x.val in [-1,1] :
         steam_lca.nw.del_conns(c01, c1)
         c01= Connection(valve, 'out1', merge_injection, 'in1')
         cond_3 = Connection(injection_source, 'out1', merge_injection, 'in2')
         #cond_4 = Connection(condensate_injection, 'out1', condensate_drum, 'in1')
-        cond_5 = Connection(merge_injection, 'out1', heat_sink, 'in1')
+        cond_5 = Connection(merge_injection, 'out1', hex_heat_sink, 'in1')
 
         #cond_6 = Connection(condensate_drum, 'out1', condensate_sink, 'in1')
-        cond_1 = Connection(heat_sink, 'out1', condensate_split, 'in1')
+        cond_1 = Connection(hex_heat_sink, 'out1', condensate_split, 'in1')
         cond_2 = Connection(condensate_split, 'out2', dummy_sink2, 'in1')
         c1 = Connection(condensate_split, 'out1', pipe_cold, 'in1')
         steam_lca.nw.add_conns(cond_1, cond_2, cond_3,cond_5,c01,c1)
@@ -201,11 +215,11 @@ def create_steam_net(steam_lca):
         steam_lca.trap =True
     
     #ean.analyse(pamb=1, Tamb=20, Chem_Ex= get_chem_ex_lib("Ahrendts"))
-    steam_lca.ean = ExergyAnalysis(steam_lca.nw, 
-            E_P=[ turbine_bus, product_bus], 
-            E_F=[fuel_bus, pump_bus, makeup_bus, ],
-            E_L=[pipe_fugitive_emission, blowdown_bus ]
-            )
+    #steam_lca.ean = ExergyAnalysis(steam_lca.nw, 
+    #        E_P=[ turbine_bus, product_bus], 
+    #        E_F=[fuel_bus, pump_bus, makeup_bus, ],
+    #        E_L=[pipe_fugitive_emission, blowdown_bus ]
+    #        )
     steam_lca.old_nw = copy.deepcopy(steam_lca.nw)
 
 if __name__ == "__main__":
